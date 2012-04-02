8dd838c287fabd07e2883816530e662646da8366
Florian Unglaub <f.unglaub@googlemail.com>
2012-04-02 08:31:11 +0200

    honor global CFLAGS

--- config.mk
+++ config.mk
@@ -9,9 +9,9 @@ MANPREFIX = ${PREFIX}/man
 
 # includes and libs
 INCS = `pkg-config --cflags libnotify`
-LIBS = `pkg-config --libs libnotify`	
+LIBS = `pkg-config --libs libnotify`
 
-CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS}
-LDFLAGS = -g ${LIBS} 
+CFLAGS += -std=c99 -pedantic -Wall -Os ${INCS}
+LDFLAGS = -g ${LIBS}
 
-CC = cc
+CC? = cc
