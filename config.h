fc7e89841372166b9f41f605e2102109d12a8bf7
Ilias-Dimitrios Vrachnis <ilias@vrachnis.com>
2013-09-03 15:55:30 +0300

    Support notifications for SD cards.
    Tweak the regular expression a bit. SD cards are using device nodes such
    as mmcsd0s1, so no notification would appear when we were looking for
    da0s1 device nodes.

--- config.h
+++ config.h
@@ -1,2 +1,2 @@
 #define SOCK_PATH "/var/run/devd.pipe"
-#define REGEX "subsystem=CDEV type=(CREATE|DESTROY) cdev=(da[0-9]+[a-z]+[0-9]?[a-z]?)"
+#define REGEX "subsystem=CDEV type=(CREATE|DESTROY) cdev=((da|mmcsd)[0-9]+[a-z]+[0-9]?[a-z]?)"
